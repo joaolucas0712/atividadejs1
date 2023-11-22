94ed732f9e875b1cc62e6107be2db72e6d7f7af3
João Lucas da Silva <133914805+joaolucas0712@users.noreply.github.com>
2023-11-22 15:37:19 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+let nome 
+console.log(nome)
+let cidade 
+console.log(cidade)
+
+nome = prompt("Digite seu nome")
+cidade = prompt("Digite sua cidade")
+
+console.log( typeof nome, typeof cidade)
+console.log(`Olá, meu nome é ${nome} e eu moro em ${cidade}`)
+
+let pergunta1
+let pergunta2
+let pergunta3
+
+pergunta1 = prompt("Você já viajou para fora do país?")
+console.log(`Você já viajou para fora do país? ${pergunta1}`)
+pergunta2 = prompt("Você gosta de programar?")
+console.log(`Você gosta de programar? ${pergunta2}`)
+pergunta3 = prompt("Você tem animais de estimação?")
+console.log(`Você tem animais de estimação? ${pergunta3}`)
+
+let y = Number(10)
+let x = Number(12)
+
+let novox = y
+let novoy = x
+
+console.log(`y = ${x} x = ${y}`)
+
+let numero1
+let numero2
+
+numero1 = prompt("digite um número")
+numero2 = prompt("digite outro número")
+
+console.log("A soma dos números é:", Number(numero1) + Number(numero2))
+console.log("A multiplicação entre eles resulta em:", Number(numero1) * Number(numero2))
+
+
+
+
+
+
